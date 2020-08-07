e799527405f79a21d901a595999b62adc02ae668
anshbest <anshulmnc123@gmail.com>
2020-08-07 06:42:22 +0530

    storing day and  daily wage in dictionary and printing key values

--- employeesalarycalculator.sh
+++ employeesalarycalculator.sh
@@ -1,41 +1,45 @@
 #!/bin/bash -x
-totalsal=0
-days=20
-empRatePerHr=40
-isPartTime=1
-isFullTime=2
-maxworkhours=100
-maxdays=20
+#!/bin/bash -x
 
-totalworkhours=0
-totaldays=0
-declare -a dailywage
-function workhours() {
-case $1 in
-        $isPartTime)
-                        empHrs=4
-                        ;;
-        $isFullTime)
-                        empHrs=8
-                        ;;
-        *)
-                        empHrs=0
-                        ;;
-esac
- echo $empHrs
+IS_PART_TIME=1
+IS_FULL_TIME=2
+MAX_HRS_IN_MONTH=100
+NUM_WORKING_DAYS=20
+EMP_RATE_PER_HR=25
+NUM_WORKING_DAYS=40
+
+totalWorkHours=0
+totalWorkingDays=0
+
+function getHours(){
+	case $1 in
+		$IS_FULL_TIME)
+			workHours=8
+			;;
+		$IS_PART_TIME)
+			workHours=4
+			;;
+		*)
+		workHours=0
+			;;
+	esac
+	echo $workHours
 }
 
-function getempwage(){
- local emphr=$1
-   echo  $((emphr*$empratePerHr))
+function calcDailyWage(){
+	local workHours=$1
+	wage=$(($workHours*$EMP_RATE_PER_HR))
+	echo $wage
 }
 
-while [[ $totalworkhours lt $maxworkhours && $totaldays lt $maxdays ]]
+while [[ $totalWorkHours -lt $MAX_HRS_IN_MONTH && $totalWorkingDays -lt $NUM_WORKING_DAYS ]]
 do
-((totaldays++))
-workhours="$(workhours $((RANDOM%3)))
-totalworkhours=(($totalworkhours + $empHrs))
-dailywage[$totaldays]=$(getempwage $empHrs)
+	((totalWorkingDays++))
+	workHours="$( getHours $((RANDOM%3)) )"
+	totalWorkHours=$(($totalWorkHours+$workHours))
+	empDailyWage[$totalWorkingDays]="$( calcDailyWage $workHours )"
 done
-sal=$(($totalworkhours * $empRatePerHr))
-echo ${dailywage[@]}
+
+totalSalary=$(($totalWorkHours*$EMP_RATE_PER_HR))
+echo "Daily Wage " ${empDailyWage[@]}
+echo " keys" ${!empDailyWage[@]}
