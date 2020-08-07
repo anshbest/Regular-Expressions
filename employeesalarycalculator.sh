fb64c9c0b7fd07056a8d48cbbb130c59f4abc780
anshbest <anshulmnc123@gmail.com>
2020-08-07 06:37:36 +0530

    storing daily wage in array

--- employeesalarycalculator.sh
+++ employeesalarycalculator.sh
@@ -1,33 +1,41 @@
 #!/bin/bash -x
+totalsal=0
+days=20
+empRatePerHr=40
+isPartTime=1
+isFullTime=2
+maxworkhours=100
+maxdays=20
 
-IS_PART_TIME=1
-IS_FULL_TIME=2
-MAX_HRS_IN_MONTH=100
-NUM_WORKING_DAYS=20
-EMP_RATE_PER_HR=20
-NUM_WORKING_DAYS=20
-
-totalWorkHours=0
-totalWorkingDays=0
-
-function getWorkingHours(){
-	case $1 in
-		$IS_FULL_TIME)
-			workHours=8
-			;;
-		$IS_PART_TIME)
-			workHours=4
-			;;
-		*)
-		workHours=0
-			;;
-	esac
-	echo $workHours
+totalworkhours=0
+totaldays=0
+declare -a dailywage
+function workhours() {
+case $1 in
+        $isPartTime)
+                        empHrs=4
+                        ;;
+        $isFullTime)
+                        empHrs=8
+                        ;;
+        *)
+                        empHrs=0
+                        ;;
+esac
+ echo $empHrs
 }
-while [[ $totalWorkHours -lt $MAX_HRS_IN_MONTH && $totalWorkingDays -lt $NUM_WORKING_DAYS ]]
+
+function getempwage(){
+ local emphr=$1
+   echo  $((emphr*$empratePerHr))
+}
+
+while [[ $totalworkhours lt $maxworkhours && $totaldays lt $maxdays ]]
 do
-	((totalWorkingDays++))
-	workHours="$( getWorkingHours $((RANDOM%3)) )"
-	totalWorkHours=$(($totalWorkHours+$workHours))
+((totaldays++))
+workhours="$(workhours $((RANDOM%3)))
+totalworkhours=(($totalworkhours + $empHrs))
+dailywage[$totaldays]=$(getempwage $empHrs)
 done
-totalSalary=$(($totalWorkHours*$EMP_RATE_PER_HR))
+sal=$(($totalworkhours * $empRatePerHr))
+echo ${dailywage[@]}
